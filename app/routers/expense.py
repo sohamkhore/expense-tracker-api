fad967959fa4e9657ddd8b75e35372a2c458c4eb
Soham1016 <sohamkhore2006@gmail.com>
2026-06-21 21:41:34 +0530

    Complete expense management and summary endpoints

--- app/routers/expense.py
+++ app/routers/expense.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.dependencies import get_db
+from sqlalchemy import func
 
-from app.schemas.expense import ExpenseCreate, ExpenseResponse
+from app.schemas.expense import ExpenseCreate, ExpenseResponse, ExpenseUpdate, ExpensePatch, ExpenseResponseTotal, ExpenseCategoryTotal
 from app.models.expense import Expense
+from datetime import date
 from app.models.category import Category
 
 router = APIRouter(
@@ -11,7 +13,7 @@ router = APIRouter(
     tags=["Expenses"]
 )
 
-
+##------------------------------------------------------------------------------------------------------##
 @router.post("/add_expense", response_model= ExpenseResponse)
 def create_expense( expense: ExpenseCreate, db: Session = Depends(get_db)):
 
@@ -41,6 +43,7 @@ def create_expense( expense: ExpenseCreate, db: Session = Depends(get_db)):
     db.refresh(new_expense)
 
     return new_expense
+##------------------------------------------------------------------------------------------------------##
 
 
 @router.get("/",response_model= list[ExpenseResponse])
@@ -49,6 +52,7 @@ def get_all_expenses(db: Session = Depends(get_db)):
     all_expenses = db.query(Expense).all()
 
     return all_expenses
+##------------------------------------------------------------------------------------------------------##
 
 
 @router.get("/{expense_id}", response_model= ExpenseResponse)
@@ -61,6 +65,7 @@ def get_expense_by_id( expense_id : int, db: Session = Depends(get_db)):
             detail = "Expense not found"
         )
     return existing
+##------------------------------------------------------------------------------------------------------##
 
 
 @router.delete("/delete/{expense_id}")
@@ -80,4 +85,134 @@ def delete_expense_by_id(expense_id : int , db: Session = Depends(get_db)):
     return {
          "message":"Expense deleted successfully"
     }
+##------------------------------------------------------------------------------------------------------##
 
+
+@router.put("/update_record/{expense_id}", response_model= ExpenseResponse)
+def update_expense_full(updated_expense : ExpenseUpdate, expense_id: int, db: Session = Depends(get_db)):
+
+    category_exists = (db.query(Category).filter(Category.id == updated_expense.category_id).first())
+    expense_record = db.query(Expense).filter(Expense.id == expense_id).first()
+    if not expense_record:
+        raise HTTPException(status_code=404, detail="Expense not found")
+
+    if not category_exists:
+        raise HTTPException(
+            status_code = 400,
+            detail = "Category not found"
+        )
+    
+    expense_record.title = updated_expense.title
+    expense_record.amount = updated_expense.amount
+    expense_record.description = updated_expense.description
+    expense_record.expense_date = updated_expense.expense_date
+    expense_record.category_id = updated_expense.category_id
+
+    
+    db.commit()
+    db.refresh(expense_record)
+
+    return expense_record
+##------------------------------------------------------------------------------------------------------##
+
+
+@router.patch("/update_record_something/{expense_id}", response_model=ExpenseResponse)
+def update_expense_something(update_expense: ExpensePatch, expense_id: int, db: Session = Depends(get_db)):
+
+    expense_exists = (db.query(Expense).filter(Expense.id == expense_id).first())
+    if not expense_exists:
+        raise HTTPException(
+            status_code=404,
+            detail="Expense not found"
+        )
+
+    update_data = update_expense.model_dump(exclude_unset=True)
+
+    # Validate category only if user is updating category_id
+    if "category_id" in update_data:
+        category_exists = (
+            db.query(Category).filter(Category.id == update_data["category_id"]).first())
+
+        if not category_exists:
+            raise HTTPException(
+                status_code=400,
+                detail="Provided Category ID does not exist"
+            )
+
+    # Update only provided fields
+    for key, value in update_data.items():
+        setattr(expense_exists, key, value)
+
+    db.commit()
+    db.refresh(expense_exists)
+
+    return expense_exists
+##------------------------------------------------------------------------------------------------------##
+
+
+@router.get("/get_expense_by_category/{category_id}",response_model= list[ExpenseResponse])
+def get_expense_by_category(category_id : int, db: Session = Depends(get_db)):
+
+    category_exists = db.query(Category).filter(Category.id == category_id).first()
+    if not category_exists :
+        raise HTTPException(
+            status_code = 400,
+            detail = "Category not found"
+        )
+    
+    all_expenses = db.query(Expense).filter(Expense.category_id == category_id).all()
+
+    return all_expenses
+##------------------------------------------------------------------------------------------------------##
+
+
+@router.get("/expense_by_date/{date}", response_model=list[ExpenseResponse])
+def get_expenses_by_date(date1: date, db:Session = Depends(get_db)):
+    expense_exist = db.query(Expense).filter(Expense.expense_date==date1).all()
+
+    if not expense_exist:
+        raise HTTPException(
+            status_code=400,
+            detail="No expense on this date"
+        )
+    return expense_exist
+##------------------------------------------------------------------------------------------------------##
+
+
+@router.get("/total_expense_ofdate/{expense_date}", response_model=ExpenseResponseTotal)
+def get_total_expenseoftheday(expense_date: date, db:Session = Depends(get_db)):
+
+    total_expense = db.query(func.sum(Expense.amount)).filter(Expense.expense_date == expense_date).scalar()
+    if total_expense is None:
+        raise HTTPException(
+            status_code=400,
+            detail="No expense on this date"
+        )
+    
+    
+    return {
+        "expense_date" :expense_date,
+        "total":float(total_expense) 
+    }
+##------------------------------------------------------------------------------------------------------##
+
+
+@router.get("/category_expense/{category_id}",response_model=ExpenseCategoryTotal)
+def category_expense_total(category_id: int, db:Session = Depends(get_db)):
+
+    exists = db.query(Category).filter(Category.id == category_id).first()
+    if not exists:
+        raise HTTPException(
+            status_code= 400,
+            detail = "Category doesnt exists"
+        )
+    
+    total_expense = db.query(func.sum(Expense.amount)).filter(Expense.category_id == category_id).scalar()
+    if total_expense is None:
+        total_expense = 0.0
+
+    return {
+        "id": category_id,
+        "title": exists.name,
+        "total": float(total_expense)
+    }
--- app/schemas/expense.py
+++ app/schemas/expense.py
@@ -21,3 +21,24 @@ class ExpenseResponse(BaseModel):
     expense_date: date
     category: CategoryResponse
     model_config = ConfigDict(from_attributes=True)
+
+class ExpenseUpdate(ExpenseBase):
+    pass
+
+class ExpensePatch(BaseModel):
+    title: str | None = None
+    amount: Decimal | None = None
+    description: str | None = None
+    expense_date: date | None = None
+    category_id: int | None = None
+
+class ExpenseResponseTotal(BaseModel):
+    expense_date: date
+    total: float
+    model_config = ConfigDict(from_attributes=True)
+
+class ExpenseCategoryTotal(BaseModel):
+    id: int
+    title: str
+    total: float
+    model_config = ConfigDict(from_attributes=True)
